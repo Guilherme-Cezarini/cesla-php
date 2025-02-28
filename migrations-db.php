f5dc220a744e4290af94ca0d981a2369d9ec8ba4
Guilherme Cezarini vieira <guilhermecezarinivieira@MacBook-Pro-de-Guilherme.local>
2025-02-27 21:16:01 -0300

    [feat] ajuste para variaveis .env nas migrations

--- migrations-db.php
+++ migrations-db.php
@@ -1,9 +1,12 @@
 <?php 
+require __DIR__. "/vendor/autoload.php";
+
+use Config\Config;
 
 return [
-    'dbname' => 'produtos',
-    'user' => 'root',
-    'password' => 'rootpassword',
-    'host' => 'localhost:3306',
+    'dbname' => Config::getEnv('DB_NAME'),
+    'user' => Config::getEnv('DB_USERNAME'),
+    'password' => Config::getEnv('DB_PASSWORD'),
+    'host' => Config::getEnv('DB_HOST'),
     'driver' => 'pdo_mysql',
 ];
